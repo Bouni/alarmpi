600161ca04a5aee23486240daea11cf867e669e7
bouni <bouni@owee.de>
2019-12-06 13:23:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 apt -y install git ansible curl
 
 # Copy ansible playbook script from github
-https://raw.githubusercontent.com/Bouni/alarmpi/master/ansible/playbooks/alarmpi.yaml -O /home/pi/alarmpi.yaml
+wget https://raw.githubusercontent.com/Bouni/alarmpi/master/ansible/playbooks/alarmpi.yaml -O /home/pi/alarmpi.yaml
 
 # Run playbook
 ansible-playbook /home/pi/alarmpi.yaml
